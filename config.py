fefbf25df8ea59ba70b4c62747b65bb923185dea
Tonga <a.gaston.gimenez@gmail.com>
2017-09-08 23:19:28 -0300

    Change the user agent and add the lyrics page url to the config file

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
-user_agent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+user_agent = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:55.0) Gecko/20100101 Firefox/55.0'
+page_url = 'https://letras.com'
