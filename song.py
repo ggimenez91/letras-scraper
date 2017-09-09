e7095d78c2ae6df464b5a75a472c298c656a7a1d
Tonga <a.gaston.gimenez@gmail.com>
2017-09-08 23:21:37 -0300

    Remove the show function and use __str__ instead.

--- song.py
+++ song.py
@@ -1,19 +1,19 @@
 class Song():
-    def __init__(self, author, title, stanzas):
+    def __init__(self, author, title, link, stanzas):
         self.author = author
         self.title = title
-        self.stanzas = stanzas
+        self.link = link
+        if stanzas != None:
+            self.stanzas = stanzas
+        else:
+            self.stanzas = []
 
     def __str__(self):
-        return str(self.__dict__)
+        result = 'Author: {0}\nTitle: {1}\nLink: {2}\n'.format(self.author,
+                self.title, self.link)
+        for stanza in self.stanzas:
+            result =+ '\n{0}'.format('\n'.join(stanza))
+        return result
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
-
-    def show(self):
-        print('Author: %s' % self.author)
-        print('Title: %s' % self.title)
-
-        for stanza in self.stanzas:
-            print('\n%s' % '\n'.join(stanza))
-
